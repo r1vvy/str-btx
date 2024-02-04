4e4c1e55b69d008f8545a202c4d78368c7742b4d
r1v <emilssefl@gmail.com>
2024-02-04 19:01:10 +0200

    add base codestyle and build jar support

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
 plugins {
     id("java")
     id("idea")
+    id("checkstyle")
 }
 
 idea {
@@ -13,13 +14,33 @@ idea {
 
 allprojects {
     apply(plugin = "java")
+    apply(plugin = "checkstyle")
     version = "1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
     }
+
+    configurations.all {
+        exclude(group = "com.google.collections", module = "google-collections")
+    }
 }
 
 subprojects {
     apply(from = "${rootProject.projectDir}/gradle/dependencies.gradle.kts")
+
+    checkstyle {
+        toolVersion = "10.13.0" // Adjust this to match your Checkstyle version
+        config = resources.text.fromFile("config/checkstyle/checkstyle.xml")
+    }
+}
+
+val uberJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("uber")
+    from(subprojects.map { it.tasks.named<Jar>("jar").flatMap { it.archiveFile } })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.named("assemble").configure {
+    dependsOn(uberJar)
 }
