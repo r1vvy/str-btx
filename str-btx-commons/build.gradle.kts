7e3bb4a8e43f89de6e05cf92838eb53935bc92bd
r1v <emilssefl@gmail.com>
2024-02-11 10:59:04 +0200

    Added jackson to commons

--- str-btx-commons/build.gradle.kts
+++ str-btx-commons/build.gradle.kts
@@ -1,2 +1,9 @@
-{
+dependencies {
+    implementation(libs.lombok)
+    implementation(libs.hibernate.validator)
+    implementation(libs.jackson.annotations)
+    implementation(libs.reactor.core)
+    implementation(libs.spring.context)
+    implementation(libs.jackson.databind)
+
 }
