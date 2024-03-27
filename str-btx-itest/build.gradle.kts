c28cb3515bedc739f7e223e89c0a37ff578c6ef2
r1v <emilssefl@gmail.com>
2024-03-27 19:35:25 +0200

    Fixed unit test setup

--- str-btx-itest/build.gradle.kts
+++ str-btx-itest/build.gradle.kts
@@ -6,7 +6,8 @@ dependencies {
     testImplementation(libs.spring.context)
     testImplementation(libs.spring.test)
     testImplementation(libs.spring.boot.starter.test)
-    testImplementation(libs.junit)
+    testImplementation(libs.junitApi)
+    testImplementation(libs.junitEngine)
     testImplementation(libs.jsonpath)
     testImplementation(libs.jsonassert)
 }
