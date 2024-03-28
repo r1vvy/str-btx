3ef62cdc42311aa965a2e65c8c2edfbd0c265b6e
r1v <emilssefl@gmail.com>
2024-03-28 20:02:28 +0200

    Added changes to GetCompanyOutResponse name

---
+++ str-btx-commons/src/main/java/com/straujupite/common/dto/out/response/GetCompanyOutResponse.java
@@ -0,0 +1,26 @@
+package com.straujupite.common.dto.out.response;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetCompanyOutResponse {
+
+    @JsonProperty("COMPANY")
+    private List<Integer> companies;
+}
+
+
+
+
+
+
